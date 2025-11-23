f4f129714867035933a862a4289a96c635348ba5
Muthuram-B <bmuthuram919@gmail.com>
2025-11-23 21:54:57 +0530

    Time: 2 ms (98.58%) | Memory: 44.1 MB (56.44%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,10 +3,11 @@ class Solution {
         int left=0;
         int right= s.length()-1;
         while(left<right){
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;/*skips the unwanted spaces and other special character*/
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
 
-             if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {return false;} left++;
+             if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {return false;}/*It checks that from the first and last using an two pointer approach if ther=y arent equal then false else it is palindrome*/
+                   left++;
              right--;
         }
         return true;
