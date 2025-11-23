a8760566c63330d509f994ced74e72e3178c2411
Muthuram-B <bmuthuram919@gmail.com>
2025-11-23 22:15:22 +0530

    Time: 2 ms (94.58%) | Memory: 48.5 MB (12.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int i=0;
-        int j=numbers.length-1;
+        int j=numbers.length-1;// 2 pointer approach
+        //it is efficient for O(1)  space complexity
         while(i<j){
-            if(numbers[i]+numbers[j]==target){
-                return new int[]{i+1,j+1};
+            if(numbers[i]+numbers[j]==target){// 2+7==9
+                return new int[]{i+1,j+1}; //[1,2]
             }if(numbers[i]+numbers[j]<target){
                 i++;
             }else{ j--;}
