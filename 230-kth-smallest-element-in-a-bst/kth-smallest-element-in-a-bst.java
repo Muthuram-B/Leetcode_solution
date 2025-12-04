19d8ebeec04a1a7058966d46f9c17fe40932eaf1
Muthuram-B <bmuthuram919@gmail.com>
2025-12-04 20:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (21.12%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -33,3 +33,66 @@ class Solution {
         inorder(root.right);
        }
 }
+/*For any BST, Inorder Traversal = Sorted Order
+
+So if we do inorder on this tree:
+
+1 → 2 → 3 → 4 → 5 → 6
+
+
+So the 3rd smallest element is:
+
+3 ✅
+
+We always go:
+👉 Left → Node → Right
+
+🔹 Step 1: inorder(5)
+
+Go LEFT → inorder(3)
+
+🔹 Step 2: inorder(3)
+
+Go LEFT → inorder(2)
+
+🔹 Step 3: inorder(2)
+
+Go LEFT → inorder(1)
+
+🔹 Step 4: inorder(1)
+
+Go LEFT → null → return
+Now VISIT 1
+
+--k → k = 2
+k == 0 ? ❌
+
+
+Go RIGHT → null → return
+
+✅ First smallest = 1
+
+🔹 Step 5: Back to node 2
+
+Now VISIT 2
+
+--k → k = 1
+k == 0 ? ❌
+
+
+Go RIGHT → null
+
+✅ Second smallest = 2
+
+🔹 Step 6: Back to node 3
+
+Now VISIT 3
+
+--k → k = 0 ✅✅✅
+k == 0 → TRUE
+result = 3 ✅
+RETURN
+
+
+✅✅✅ We FOUND the 3rd smallest element → 3
+*/
