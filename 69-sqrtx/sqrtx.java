996a14939b8daa6aae10758bba02375b883c171e
Muthuram-B <bmuthuram919@gmail.com>
2026-01-20 19:08:16 +0530

    Time: 1 ms (96.49%) | Memory: 42.5 MB (83.93%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        // double n=x;;
+        // double root;
+        // while(true){
+        //     root=0.5*(n+(x/n));
+        //     if(Math.abs(root-n)<1){
+        //         break;
+        //     }
+        //     n=root;
+        // }
+        // return (int)root;
+
+
+        if(x==0||x==1) return x;
+        int s=1; int e=x;
+
+        int ans=0;
+
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(m*m==x) return m;
+            if(m<=x/m){
+                s=m+1;
+                ans=m;
+            }else e=m-1;
+        }
+        return ans;
+    }
+}
