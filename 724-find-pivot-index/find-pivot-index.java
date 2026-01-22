79ca7a4055e1de153c16a049731165855a325d06
Muthuram-B <bmuthuram919@gmail.com>
2026-01-22 18:47:04 +0530

    Time: 1 ms (97.99%) | Memory: 46.8 MB (98.87%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -14,3 +14,14 @@ class Solution {
        return -1;
     }
 }
+/* it initally totals to the rightsum eg: 1+7+3+6+5+6=28
+then it removes form the starting element element eg = 28-1=27 and
+then it checks 0==27 , its not
+again it continues the next element 27-7=20 and 
+then it checks 1==20 ,its not and it add to left sum=1+7=8
+again it checks   20-3=17 and it 
+checks 8==17 ,its not and then it add to left =8+3=11
+again it continues 17-6==11 and it checks
+11==11 then it return the current index which is 3 here
+
+in this way it takes O{n} and no extra space*/
