1a791a4be6c53af151e6c793c7bccc7b61e675de
Muthuram-B <bmuthuram919@gmail.com>
2026-02-05 22:52:37 +0530

    Time: 20 ms (81.92%) | Memory: 56.3 MB (87.60%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -9,8 +9,12 @@ class Solution {
         if(people[l]+people[r]<=limit){
             l++;
         }
-        r--;
-
+    /* 1,2,2,3   here first and second gives 4 but limit 3 so 3 at the end can be put
+    in the separate boat and r--
+      next 1 + 2=3  so l++  and then r--   1,2 in  a second boat
+      then 2 can be put in separte boat and boat count bexomes  3  
+      */
+        r--;//each and every time it moves after the person is put in it with limit
         boat++;
      }   
      return boat;
