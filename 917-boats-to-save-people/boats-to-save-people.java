46199ecf7d9c29c9736aa9c22036df1595d4eafa
Muthuram-B <bmuthuram919@gmail.com>
2026-02-05 22:47:21 +0530

    Time: 20 ms (81.92%) | Memory: 56.7 MB (34.87%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+
+     Arrays.sort(people);
+       int boat=0;
+     int l=0; int r=people.length-1;
+     while(l<=r){
+
+        if(people[l]+people[r]<=limit){
+            l++;
+        }
+        r--;
+
+        boat++;
+     }   
+     return boat;
+    }
+}
