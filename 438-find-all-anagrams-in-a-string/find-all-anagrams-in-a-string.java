6bb5bdfb7263467951f839b022432d37319d4153
Muthuram-B <bmuthuram919@gmail.com>
2026-02-09 19:28:04 +0530

    Time: 13 ms (51.33%) | Memory: 46.8 MB (71.31%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
+        //it follows sliding window + freq mapping
         int[] pCount=new int[26];
         int[] sCount=new int[26];
         List<Integer> res =new ArrayList<>();
         
         for(char c:p.toCharArray()){
-            pCount[c-'a']++;
+            pCount[c-'a']++;// it adds the freq of ele in p string
         }
         for(int i=0;i<s.length();i++){
-             sCount[s.charAt(i)-'a']++;
+             sCount[s.charAt(i)-'a']++; // it adds every ele to the scount array
 
              if(i>=p.length()) sCount[s.charAt(i-p.length())-'a']--;
-
+ // it removes the ele if i exceeds p that is here the 3 ele  it remove the first ele
              if(Arrays.equals(pCount,sCount)) res.add(i-p.length()+1);
 
-             
+           //.equals method compares the length if same then check the ele at the
+           // idx if both are same return the idx of the starting point             
             
         }
         return res;
