08d4484d0d345077bf9bd941b2c6c08040ba4019
Muthuram-B <bmuthuram919@gmail.com>
2026-03-02 23:02:20 +0530

    Time: 107 ms (39.39%) | Memory: 142 MB (64.31%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -6,13 +6,15 @@ private int[][] prefix;
         int m=matrix.length;
         int n=matrix[0].length;
         prefix=new int[m][n];
-
+         /* here it uses the prefixsum to calculate the values in the matrix
+         that it finds the sum of each element for every matrix idx and  add the top,left and topleft ele is subracted beacause eg 3 0
+                                     5  6   here the sum of mat[2][2]= 3+8+6-3(which is added with both 3+0 and 5+3 so we subtract once)=14*/
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int top=(i>0)?prefix[i-1][j]:0;
                 int left=(j>0)?prefix[i][j-1]:0;
                 int topleft=(i>0 && j>0)?prefix[i-1][j-1]:0;
-
+                   // this is repeated for each ele and it is continued for each idx
                 prefix[i][j]=matrix[i][j]+top+left-topleft;
 
 
@@ -21,6 +23,10 @@ private int[][] prefix;
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
+        /* this takes the ele of particular mat[i][j] to certain mat[i][j]
+        it takes the total which is the end of mat and top which is the remaining part 
+        and left which is the left part  and topleft is added beause it is used twice
+        so by this we find the sum of the particular idx of the matix in the whole matrix*/
         int total=prefix[row2][col2];
         int top=(row1>0)?prefix[row1-1][col2]:0;
         int left=(col1>0)?prefix[row2][col1-1]:0;
