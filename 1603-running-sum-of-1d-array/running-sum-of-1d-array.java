ad8ab8e171e6f192bed2fd00d07966b35a527664
Muthuram-B <bmuthuram919@gmail.com>
2026-03-03 18:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (94.53%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        //int[] prefixsum=new int[nums.length+1];
+        for(int i=1;i<nums.length;i++){
+           nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+}
