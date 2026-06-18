d4bc414b46bda58e5c3304ffe68e02145c632203
Muthuram-B <bmuthuram919@gmail.com>
2026-06-19 02:19:09 +0530

    Time: 15 ms (26.57%) | Memory: 49.9 MB (35.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -18,40 +18,41 @@ class Solution {
 
     if(strs==null || strs.length==0) return new ArrayList<>();
 
-    // Map<String,List<String>> freqmap=new HashMap<>();
+    Map<String,List<String>> freqmap=new HashMap<>();
 
-    // for(String str:strs){
+    for(String str:strs){
 
-    //     String freqstr=getFreqStr(str);
+        String freqstr=getFreqStr(str);
 
-    //     if(freqmap.containsKey(freqstr)) freqmap.get(freqstr).add(str);
+        if(freqmap.containsKey(freqstr)) freqmap.get(freqstr).add(str);
 
-    //     else{
-    //         List<String> s = new ArrayList<>();
-    //         s.add(str);
-    //         freqmap.put(freqstr,s);
-    //     }
-    // }
-    // return new ArrayList<>(freqmap.values());
-    // }
+        else{
+            List<String> s = new ArrayList<>();
+            s.add(str);
+            freqmap.put(freqstr,s);
+        }
+    }
+    return new ArrayList<>(freqmap.values());
+    }
 
 
-    // private String getFreqStr(String str){
+    private String getFreqStr(String str){
 
-    //     int[] freq=new int[26];
+        int[] freq=new int[26];
 
-    //     for(char c:str.toCharArray()) freq[c-'a']++;
+        for(char c:str.toCharArray()) freq[c-'a']++;
 
-    //     StringBuilder freqstr=new StringBuilder("");
-    //     char c='a';
-    //     for(int i:freq){
-    //         freqstr.append(c);
-    //         freqstr.append(i);
-    //         c++;
-    //     }
+        StringBuilder freqstr=new StringBuilder("");
+        char c='a';
+        for(int i:freq){
+            freqstr.append(c);
+            freqstr.append(i);
+            c++;
+        }
 
-    //     return freqstr.toString();
-    // }
+        return freqstr.toString();
+    }
+}
 
 
 
@@ -60,26 +61,26 @@ class Solution {
       then check if the number is present if it is then add the respective word to that specific unique number 
       finally return the values of the unique numbers in group
 */
-      int nos[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103};
+//       int nos[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103};
 
-      Map<BigInteger,List<String>> map=new HashMap<>();
-      for(String str:strs){
-        BigInteger prod=BigInteger.ONE;
-        for(int i=0;i<str.length();i++){
-            //prod*=nos[str.charAt(i)-'a'];
-            prod = prod.multiply(BigInteger.valueOf(nos[str.charAt(i) - 'a']));
-        }
-        if(map.containsKey(prod)) map.get(prod).add(str);
+//       Map<BigInteger,List<String>> map=new HashMap<>();
+//       for(String str:strs){
+//         BigInteger prod=BigInteger.ONE;
+//         for(int i=0;i<str.length();i++){
+//             //prod*=nos[str.charAt(i)-'a'];
+//             prod = prod.multiply(BigInteger.valueOf(nos[str.charAt(i) - 'a']));
+//         }
+//         if(map.containsKey(prod)) map.get(prod).add(str);
 
-        else{
-            map.put(prod,new ArrayList<String>());
-            map.get(prod).add(str);
-        }
-      }
+//         else{
+//             map.put(prod,new ArrayList<String>());
+//             map.get(prod).add(str);
+//         }
+//       }
 
-      List<List<String>> list=new ArrayList<List<String>>();
-     for(List<String> res:map.values()) list.add(res);
+//       List<List<String>> list=new ArrayList<List<String>>();
+//      for(List<String> res:map.values()) list.add(res);
 
-     return list;
-}
-}
+//      return list;
+// }
+// }
