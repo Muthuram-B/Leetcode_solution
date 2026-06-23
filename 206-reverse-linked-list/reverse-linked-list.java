be3a099776f6fd3421968077c9a159b6f39b8c64
Muthuram-B <bmuthuram919@gmail.com>
2026-06-24 01:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (67.07%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -15,19 +15,39 @@ class Solution {
 
 // push the value in stack and then retrieve it as a Node and return it
 
- Stack<Integer> stack=new Stack<>();
- while(head!=null){
-    stack.push(head.val);
-    head=head.next;
- }
+//  Stack<Integer> stack=new Stack<>();
+//  while(head!=null){
+//     stack.push(head.val);
+//     head=head.next;
+//  }
 
- ListNode reverselist=new ListNode(-1);
- ListNode ptr=reverselist;
+//  ListNode reverselist=new ListNode(-1);
+//  ListNode ptr=reverselist;
 
- while(!stack.isEmpty()){
-    ptr.next=new ListNode(stack.pop());
-    ptr=ptr.next;
- }
- return reverselist.next; 
-    }
+//  while(!stack.isEmpty()){
+//     ptr.next=new ListNode(stack.pop());
+//     ptr=ptr.next;
+//  }
+//  return reverselist.next; 
+//     }
+
+// to do without an extra space that is modifying the listNode itself
+// it just make the pointers to map in reverse direction
+
+if(head==null) return null;
+if(head.next==null) return head;
+
+ListNode prevnode= null;
+ListNode currnode =head;
+while(currnode!=null){
+    ListNode nextnode=currnode.next;
+    currnode.next=prevnode;
+    prevnode=currnode;
+    currnode=nextnode;
+}
+head=prevnode;
+
+return head;
+}
+
 }
