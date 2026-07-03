46bf0f296ccfdf751f6d72224a894f7dd842c5be
Muthuram-B <bmuthuram919@gmail.com>
2026-07-04 04:52:25 +0530

    Time: 2 ms (99.53%) | Memory: 43.5 MB (38.10%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -7,9 +7,9 @@ class Solution {
         int p2=0; int p3=0; int p5=0;
         for(int i=1;i<n;i++){
 
-            int twomultiplier = ugly[p2]*2;
-            int threemultiplier = ugly[p3]*3;
-            int fivemultiplier =ugly[p5]*5;
+            int twomultiplier = ugly[p2]*2; //1*2=2 -> 2*2=4 -> 2*2=4 -> 4*2=8
+            int threemultiplier = ugly[p3]*3;//1*3=3 -> 1*3=3 -> 2*3=6 -> 2*3=6
+            int fivemultiplier =ugly[p5]*5; // 1*5=5 -> 1*5=5 -> 1*5=5 -> 2*2=4
 
         ugly[i] =Math.min(twomultiplier,Math.min(threemultiplier,fivemultiplier));
 
