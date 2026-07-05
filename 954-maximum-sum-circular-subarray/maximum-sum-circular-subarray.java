517b58eb1861b883bf1dfced2397a649b6b435be
Muthuram-B <bmuthuram919@gmail.com>
2026-07-05 17:47:46 +0530

    Time: 7 ms (46.10%) | Memory: 51.5 MB (9.79%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -10,11 +10,18 @@ class Solution {
             total+=nums[i];
             currmax=Math.max(nums[i],nums[i]+currmax);
             maxsum=Math.max(maxsum,currmax);
+            /* eg 5,-3,5 the masum=7 after every iteration since it is circular
+            we use the logic minsum(it keep track of min ele possible)
+            minsum=-3  and if we do total =7 and total-minsum (7-(-3)=10) */
 
             currmin=Math.min(nums[i],nums[i]+currmin);
             minsum=Math.min(minsum,currmin);
         }
-        if(maxsum<0) return maxsum;
+        if(maxsum<0) return maxsum; // edge case for all negative integers
+
+        /*eg = -5,-3,-5   the maxsum = -3
+          total = -13 and minsum=-13  total-minsum=0 to handle that we just
+          return the maxsum it holds the least negative integer*/
 
         return Math.max(maxsum,total-minsum);
     }
